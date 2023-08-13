028108dbb52bd97ef7d61445337fbde7ba9449c6
LL zhang <zhang31666@outlook.com>
2023-08-13 12:46:14 +0800

    新增域通用模块

---
+++ Z.DomainCommons/Models/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z.DomainCommons.Models
+{
+    public interface IEntity
+    {
+        public Guid Id { get; }
+        //public long AutoIncId { get; }//在MySQL中，用Guid做物理主键性能非常低。因此，物理上用自增的列做主键。但是逻辑上、包括两表之间的关联都用Guid。
+    }
+}
