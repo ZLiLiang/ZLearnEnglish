542d5d1f40803d26484af9d782b2a2d52cf7ee56
LL zhang <zhang31666@outlook.com>
2023-08-14 14:56:28 +0800

    新增事件总线、RabbitMQ

---
+++ Z.EventBus/DynamicIntegrationEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using Dynamic.Json;
+
+namespace Z.EventBus
+{
+    public abstract class DynamicIntegrationEventHandler : IIntegrationEventHandler
+    {
+        /// <summary>
+        /// 动态类型整合事件柄
+        /// </summary>
+        /// <param name="eventName"></param>
+        /// <param name="eventData"></param>
+        /// <returns></returns>
+        public Task Handle(string eventName, string eventData)
+        {
+            dynamic dynamicEventData = DJson.Parse(eventData);
+            return HandleDynamic(eventName, dynamicEventData);
+        }
+
+        public abstract Task HandleDynamic(string eventName, dynamic eventData);
+    }
+}
---
+++ Z.EventBus/EventNameAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Z.EventBus
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class EventNameAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public EventNameAttribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
