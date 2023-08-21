51cb70b0c719099a2b17002c77a4381f913f0c7e
LL zhang <zhang31666@outlook.com>
2023-08-21 23:20:06 +0800

    新增搜索服务API

---
+++ Z.SearchService.WebAPI/Program.cs
@@ -0,0 +1,34 @@
+using Z.CommonInitializer;
+using Z.SearchService.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.ConfigureDbConfiguration();
+builder.ConfigureExtraServices(new InitializerOptions
+{
+    LogFilePath = "e:/temp/SearchService.log",
+    EventBusQueueName = "SearchService.WebAPI"
+});
+// Add services to the container.
+
+builder.Services.Configure<ElasticSearchOptions>(builder.Configuration.GetSection("ElasticSearch"));
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "SearchService.WebAPI", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SearchService.WebAPI v1"));
+}
+app.UseZDefault();
+app.MapControllers();
+
+app.Run();
+
